09a1e70be96c1fcc574413d5ed392c00c19eed33
Adam Tang <ya4dang1@gmail.com>
2020-07-01 11:09:19 +0800

    Test React with Jest Snapshot Tests

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -45,7 +45,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.post("/courses/", function(req, res, next) {
+server.post("/books/", function(req, res, next) {
   const error = validateCourse(req.body);
   if (error) {
     res.status(400).send(error);
