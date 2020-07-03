bb8be7195b2ff3cc046c726598ca214541152363
Adam Tang <ya4dang1@gmail.com>
2020-07-03 14:52:11 +0800

    Production Build

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,23 +1,7 @@
-import thunk from "redux-thunk";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import { createLogger } from "redux-logger";
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
-
-let middleware = [thunk, reduxImmutableStateInvariant()];
-if (process.env.NODE_ENV === "development") {
-  const logger = createLogger({
-    collapsed: true
-  });
-  middleware = [...middleware, logger];
-}
-
-export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(...middleware))
-  );
+/* eslint-disable global-require */
+// Use CommonJS require below so we can ddynamically import during build-time.
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./configureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
 }
